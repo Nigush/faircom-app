61c51d66c7e67e5aaea79483b2a7525ca2c121dc
faircom <nigushik96@gmail.com>
2020-07-31 13:20:35 +0300

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wnh%d947xywq*1icr2)td0&qp_-n%@-d$*04o_p5_bi4*^q(%u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'www.ffclog.org',
