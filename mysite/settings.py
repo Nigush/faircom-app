7f2e0fcdf69bbfab09912497b29bf0e3caee3c2c
faircom <nigushik96@gmail.com>
2020-07-31 16:44:09 +0300

    Changed the HTML

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip("/"))
+
+STATICFILES_DIRS = (
+    '/static/',
+    '/upload/',
+)
